9d4ac66072dabcf0596c2ef1245573e90e64e77b
separk-1 <78338116+separk-1@users.noreply.github.com>
2023-08-20 20:12:16 +0900

    log

--- 5_status.py
+++ 5_status.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from colorama import init, Fore
+from colorama import Fore
 import argparse
 
 def extract_date(date_string):
@@ -8,53 +8,45 @@ def extract_date(date_string):
     month = int(date_string[-4:-2])
     day = int(date_string[-2:])
     year = 2000 + int(date_string[:-4])
-
     date_obj = datetime(year, month, day)
     weekdays_kr = ["월", "화", "수", "목", "금", "토", "일"]
-    weekday = weekdays_kr[date_obj.weekday()]
+    return f"{year-2000}년 {month}/{day}({weekdays_kr[date_obj.weekday()]})", date_obj
 
-    # formatted_date와 date_obj를 모두 반환합니다.
-    return f"{year-2000}년 {month}/{day}({weekday})", date_obj
-
-def count_files_in_directory(path):
+def count_files_in_directory(path, output_file=None):
     if not os.path.exists(path):
         print("지정된 경로가 존재하지 않습니다.")
         return
-
     if not os.path.isdir(path):
         print("지정된 경로는 디렉토리가 아닙니다.")
         return
 
+     # Directory path 정보를 txt 파일 맨 앞에 추가
+    if output_file:
+        output_file.write(f"Directory Path: {path}\n\n")
+
     folder_dict = {}
+    for foldername, _, filenames in os.walk(path):
+        parts = foldername.split(os.path.sep)
+        if len(parts) < 3:
+            continue
 
-    for foldername, subfolders, filenames in os.walk(path):
-            parts = foldername.split(os.path.sep)
-            
-            if len(parts) >= 3:
-                main_folder = parts[-2]
-                date_folder = parts[-1]
+        main_folder = parts[-2]
+        date_folder = parts[-1].split('_')[-1]
 
-                # date_folder의 형식이 D1_210203인 경우, 숫자 부분만 추출
-                if '_' in date_folder:
-                    date_folder = date_folder.split('_')[1]
-                
-                if main_folder.startswith("D") and date_folder.isdigit():
-                    if main_folder not in folder_dict:
-                        folder_dict[main_folder] = []
-                    formatted_date, date_obj = extract_date(date_folder)
-                    folder_dict[main_folder].append((formatted_date, date_obj, len(filenames)))
+        if main_folder.startswith("D") and date_folder.isdigit():
+            formatted_date, date_obj = extract_date(date_folder)
+            folder_dict.setdefault(main_folder, []).append((formatted_date, date_obj, len(filenames)))
 
-    # 폴더별 정보 출력
-    for main_folder in sorted(folder_dict.keys()):
-        # 실제 datetime 객체를 기반으로 정렬합니다.
+    for main_folder in sorted(folder_dict):
         dates = sorted(folder_dict[main_folder], key=lambda x: x[1])
-        
         subfolder_count = len(dates)
         percentage = subfolder_count / 208 * 100
-        color = Fore.RESET
-
-        print(f"{color}{main_folder}({percentage:.2f}%)")
         
+        message = f"{main_folder}({percentage:.2f}%)"
+        print(message)
+        if output_file:
+            output_file.write(message + '\n')
+
         for date_str, _, count in dates:
             # 데이터 개수에 따른 색상 설정
             if count > 800:
@@ -65,24 +57,40 @@ def count_files_in_directory(path):
                 color = Fore.RED
             else:
                 color = Fore.RESET
-
-            print(f"{color}- {date_str}: {count}")
+        
+            message = f"{color}- {date_str}: {count}{Fore.RESET}"
+            print(message)
+            if output_file:
+                output_file.write(f"- {date_str}: {count}\n")
+        
         
         print()
+        if output_file:
+            output_file.write('\n')
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description='computer (home/lab)')
-    parser.add_argument('-c', '--com', type=str, required=False, default='lab', help='실행할 모드를 설정합니다.')
+    parser = argparse.ArgumentParser(description='Specify computer type')
+    parser.add_argument('-c', '--com', choices=['lab', 'l', 'home', 'h', 'drive', 'd'], default='lab', help='실행할 모드를 설정합니다.')
+    parser.add_argument('-l', '--log', action='store_true', help='log 저장 여부를 설정합니다.')
     args = parser.parse_args()
 
-    if args.com in ['lab', 'l']:
-        directory_path = "G:/내 드라이브/0. Run/1. Research/2. HangingObjectDetection/HOD/output"
-    elif args.com in ['home', 'h']:
-        directory_path = "H:/내 드라이브/0. Run/1. Research/2. HangingObjectDetection/HOD/output"
-    elif args.com in ['drive', 'd']:
-        directory_path = "D:/images"
-    else:
+    paths = {
+        'lab': "G:/내 드라이브/0. Run/1. Research/2. HangingObjectDetection/HOD/output",
+        'l': "G:/내 드라이브/0. Run/1. Research/2. HangingObjectDetection/HOD/output",
+        'home': "H:/내 드라이브/0. Run/1. Research/2. HangingObjectDetection/HOD/output",
+        'h': "H:/내 드라이브/0. Run/1. Research/2. HangingObjectDetection/HOD/output",
+        'drive': "D:/images",
+        'd': "D:/images"
+    }
+    
+    directory_path = paths.get(args.com, None)
+    if not directory_path:
         print("wrong mode")
+        exit()
 
-    count_files_in_directory(directory_path)
+    if args.log:
+        with open('results.txt', 'w', encoding='utf-8') as f:
+            count_files_in_directory(directory_path, f)
+    else:
+        count_files_in_directory(directory_path)
+
