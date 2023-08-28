98e33a1dbb33882d6d6b955a067faa376bd94207
separk-1 <78338116+separk-1@users.noreply.github.com>
2023-08-29 00:19:49 +0900

    h

--- get_dataset.py
+++ get_dataset.py
@@ -1,25 +1,8 @@
 import shutil
-# 원본 txt 파일을 읽기 모드로 열기
-with open('file_list.txt', 'r') as f:
-    lines = f.readlines()
-
-# 변환된 파일명을 저장할 리스트 생성
-new_lines = []
-
-# 각 줄을 새로운 형식으로 변환
-for line in lines:
-    line = line.strip()  # 줄바꿈 문자 제거
-    parts = line.split('_')  # 언더스코어(_)로 문자열을 분리
-    new_line = f"{parts[0]}/{parts[0]}_{parts[1]}/{parts[0]}_{parts[1]}_{parts[2]}_{parts[3][0]}.png"  # 새로운 형식으로 조립
-    new_lines.append(new_line)  # 리스트에 추가
-
-# 새 txt 파일을 쓰기 모드로 열어 변환된 파일명을 저장
-with open('file_list.txt', 'w') as f:
-    for new_line in new_lines:
-        f.write(f"{new_line}\n")
+import os
 
 # 원본 txt 파일을 읽기 모드로 열기
-with open('file_list.txt', 'r') as f:
+with open('file_list_modified.txt', 'r') as f:
     files = f.read().splitlines()
 
 # 복사할 원본 파일의 기본 경로
@@ -29,12 +12,28 @@ source_folder = "D:/Dataset/Site/images/"
 destination_folder = "I:/Dataset_sample/images/"
 
 # 파일 목록을 돌면서 파일 복사하기
+not_found_files = []  # not found된 파일을 저장할 리스트
+
 for file_path in files:
     source = source_folder + file_path
     file_name = file_path.split("/")[-1]
     destination = destination_folder + file_name
 
-    # 실제 파일 복사 수행
-    shutil.copy(source, destination)
+    # 대상 경로에 파일이 이미 있는지 확인
+    if not os.path.exists(destination):
+        try:
+            # 실제 파일 복사 수행
+            shutil.copy(source, destination)
+        except FileNotFoundError:
+            print(f"File {file_name} not found. Skipping.")
+            not_found_files.append(file_name)
+
+# not found된 파일 목록을 정렬
+not_found_files.sort()
+
+# 정렬된 not found된 파일 목록을 txt 파일로 저장
+with open('not_found_files.txt', 'w') as f:
+    for file_name in not_found_files:
+        f.write(f"{file_name}\n")
 
 print("Files have been copied.")
